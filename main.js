4a7aed3c4a741f14bef4572362bbf9bdb31ec16d
NowWeAre-Free <kontodarm@wp.pl>
2023-07-03 21:30:50 +0200

    switch option modified, cleaned

--- main.js
+++ main.js
@@ -4,39 +4,35 @@ let color = 100;
 
 document.body.style.backgroundColor = `rgb(${color}, ${color}, ${color})`;
 
+// const changeColor = (e) => {
+//   console.log(e.keyCode, e.which);
+//   console.log(color);
+//   // 38 - strzałka do góry
+//   // 40 - strzalka w dół
+
+//   // wciśnięcie klawisza strzałki do góry ma zmieniać kolor (stopniowo) na biały czyli rgb(255,255,255). Wciśniecie klawisza strzałki w dół ma zmienić kolor stopniowo na czarny (docelowo rgb(0,0,0))
+//   //tutaj twój kod
+//   // wersja 1 - instrukcja if
+//   // wersja 2 - instrukcja switch
+
+//     if (e.keyCode === 38 && color < 255) {
+//       color += 5;
+//     } else if (e.keyCode === 40 && color >= 0) {
+//       color -= 5;
+//     }
+//     document.body.style.backgroundColor = `rgb(${color}, ${color}, ${color})`;
+//   };
+
 const changeColor = (e) => {
-  console.log(e.keyCode, e.which);
-  console.log(color);
-  // 38 - strzałka do góry
-  // 40 - strzalka w dół
-
-  // wciśnięcie klawisza strzałki do góry ma zmieniać kolor (stopniowo) na biały czyli rgb(255,255,255). Wciśniecie klawisza strzałki w dół ma zmienić kolor stopniowo na czarny (docelowo rgb(0,0,0))
-  //tutaj twój kod
-  // wersja 1 - instrukcja if
-  // wersja 2 - instrukcja switch
-
-  if (e.keyCode === 38 && color < 255) {
-    color += 5;
-  } else if (e.keyCode === 40 && color >= 0) {
-    color -= 5;
+  switch (e.keyCode) {
+    case 38:
+      color <= 255 ? (color += 5) : color;
+      break;
+    case 40:
+      color >= 0 ? (color -= 5) : color;
+      break;
   }
   document.body.style.backgroundColor = `rgb(${color}, ${color}, ${color})`;
 };
-
-//   switch (e.keyCode) {
-//     case 38:
-//       // document.body.style.backgroundColor = `rgb(${(red += 5)}, ${(green += 5)}, ${(blue += 5)})`
-//       red++;
-//       green++;
-//       blue++;
-//       break;
-//     case 40:
-//       // document.body.style.backgroundColor = `rgb(${(red -= 5)}, ${(green -= 5)}, ${(blue -= 5)})`;
-//       red -= 5;
-//       green -= 5;
-//       blue -= 5;
-//       break;
-//   }
-// };
 
 window.addEventListener("keydown", changeColor);
